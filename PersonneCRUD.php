dc7bc0dcdc08694b115defb11cb4556af6cd1c92
itahriouan zakaria <zakariaitahriouan@gmail.com>
2025-01-10 10:38:20 +0100

    version 10/01

--- PersonneCRUD.php
+++ PersonneCRUD.php
@@ -7,17 +7,54 @@ class PersonneCRUD {
         $this->pdo = new PDO("mysql:host=localhost;dbname=test2024","root","");
 
     }
-    public function getAll(){
+    /**
+     * @return Personne[]
+     */
+    public function getAll(): array{
         try{
             $pdo = $this->pdo;
                 $stm = $pdo->prepare("select * from personne");
                 $stm->execute();
                 return $stm->fetchAll(PDO::FETCH_CLASS,'Personne');
-            
             }
             catch(PDOException $e){
                 echo $e->getMessage();
                 die();
             }
     }
+    public function create(Personne $personne){
+        try{
+            $pdo = $this->pdo;
+                $stm = $pdo->prepare("insert into personne VALUES(DEFAULT,?,?,?)");
+                $stm->execute([$personne->getNom(),$personne->getPrenom(),$personne->getEmail()]);
+            }
+            catch(PDOException $e){
+                echo $e->getMessage();
+                die();
+            }
+    }
+    public function update(Personne $personne){
+        try{
+            $pdo = $this->pdo;
+                $stm = $pdo->prepare("update personne set nom=?, prenom=?, email=? where id=?");
+                $stm->execute([$personne->getNom(),$personne->getPrenom(),$personne->getEmail(),$personne->getId()]);
+            }
+            catch(PDOException $e){
+                echo $e->getMessage();
+                die();
+            }
+    }
+    public function delete(Personne $personne){
+        try{
+            $pdo = $this->pdo;
+                $stm = $pdo->prepare("delete from personne where id=?");
+                $stm->execute([$personne->getId()]);
+            }
+            catch(PDOException $e){
+                echo $e->getMessage();
+                die();
+            }
+    }
+    
+
 }
--- index.php
+++ index.php
@@ -5,7 +5,26 @@ require 'PersonneCRUD.php';
 
 $PCRUD = new PersonneCRUD();
 
-$personnes = $PCRUD->getAll();
+// $p1 = new Personne();
+// $p1->setNom("bakkali");
+// $p1->setPrenom("imad");
+// $p1->setEmail("bakkali@gmail.com"); 
+// $PCRUD->create($p1);
 
-var_dump($personnes);
+// $personnes = $PCRUD->getAll();
+// var_dump($personnes);
 
+// $p1 = $personnes[0];
+// $p1->setNom("ITAHRIOUAN");
+// $p1->setPrenom("ZAKARIA");
+// $PCRUD->update($p1);
+// echo "après modification";
+//  var_dump($personnes);
+
+ $personnes = $PCRUD->getAll();
+ var_dump($personnes);
+ $p1 = $personnes[1];
+ $PCRUD->delete($p1);
+ echo "après suppression";
+ $personnes = $PCRUD->getAll();
+ var_dump($personnes);
